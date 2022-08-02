5afbf94432b2b402bd757863408bd08a70c8927c
Will Hawks <willkhawks@gmail.com>
2022-08-02 13:29:38 -0500

    add gtag

--- components/Meta.js
+++ components/Meta.js
@@ -5,6 +5,17 @@ const Meta = () => {
   return (
     <Head>
       <title>Move To Learn TV</title>
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-8NWL5MECMJ" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8NWL5MECMJ');
+        `}
+      </Script>
     </Head>
   );
 };
